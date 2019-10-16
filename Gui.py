f09bd3953c532581cfc296677fd95eff21974dea
Wuriqilang <543677991@qq.com>
2019-10-16 23:37:59 +0800

    修改

--- Gui.py
+++ Gui.py
@@ -2,6 +2,8 @@ import tkinter
 
 root = tkinter.Tk()
 #Python3 需要用这中格式编程了 愈来越向C#
-myLabel=tkinter.Label(root,text="Welcome to python's World")
+myLabel = tkinter.Label(root, text="Welcome to python's World")
 myLabel.pack()
+
+
 root.mainloop()
